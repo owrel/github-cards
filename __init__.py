f12eff3e2d9b7754cbe66f649038d1ee8ddf3865
Owrel <aurelien.simon49@gmail.com>
2023-12-15 11:24:51 +0100

    Restructure

--- __init__.py
+++ __init__.py
@@ -1,14 +1,15 @@
 import requests
+import toolbox as tb
 
 
+@tb.decorator.memoize(94000) 
 def get_github_repo_info(username, repo_name):
     url = f'https://api.github.com/repos/{username}/{repo_name}'
     response = requests.get(url)
     if response.status_code == 200:
         repo_info = response.json()
-        print(repo_info)
+        return repo_info()
     else:
-        # Print an error message if the request was not successful
         print(
             f"Error: Unable to fetch information. Status code: {response.status_code}")
 
@@ -16,4 +17,5 @@ def get_github_repo_info(username, repo_name):
 # Example usage
 github_username = 'owrel'
 repository_name = 'clindoc'
+get_github_repo_info(github_username, repository_name)
 get_github_repo_info(github_username, repository_name)
